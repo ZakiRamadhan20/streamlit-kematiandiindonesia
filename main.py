f3da7cfb2d619aaf67958bb4f16c26edfc1d297d
ZakiRamadhan20 <148049942+ZakiRamadhan20@users.noreply.github.com>
2025-01-12 23:12:50 +0700

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
+import seaborn as sn
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier, plot_tree
