9d3b9eb1c45e41f98863213dd70d83d261703294
ZakiRamadhan20 <148049942+ZakiRamadhan20@users.noreply.github.com>
2025-01-12 23:08:12 +0700

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
